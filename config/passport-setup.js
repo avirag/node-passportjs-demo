c26f1365a56e6b18e5039ca695d58c8efbf7a313
Anita Virag <ik.virag@gmail.com>
2019-03-11 23:58:19 +0100

    redirect URI

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -6,10 +6,11 @@ const keys = require('./keys');
 passport.use(
   new GoogleStrategy({
   // options for the google GoogleStrategy
+  callbackURL: '/auth/google/redirect',
   clientID: keys.google.clientID,
   clientSecret: keys.google.clientSecret
-  }), () => {
+  }, () => {
     // passport callback function
 
-  }
+  })
 );
