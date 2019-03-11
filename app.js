98d19311d6f83dfffc979ff7f41a7e304a693aab
Anita Virag <ik.virag@gmail.com>
2019-03-12 00:30:41 +0100

    making a user model

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const authRoutes = require('./routes/auth-routes');
+const mongoose = require('mongoose');
+
 const passportSetup = require('./config/passport-setup');
+const keys = require('./config/keys');
 
 const app = express();
 
 // view engine
 app.set('view engine', 'ejs');
+
+// connect to  mongodb
+mongoose.connect(keys.mongodb.dbURI, () => {
+  console.log('connected to mongoDB');
+});
 
 // set up routes
 app.use('/auth', authRoutes);
--- config/passport-setup.js
+++ config/passport-setup.js
@@ -9,7 +9,7 @@ passport.use(
   callbackURL: '/auth/google/redirect',
   clientID: keys.google.clientID,
   clientSecret: keys.google.clientSecret
-  }, () => {
+  }, (accesssToken, refreshToken, profile, done) => {
     // passport callback function
 
   })
